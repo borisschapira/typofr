3c890dab00aa031f8fc9b860caab7ea2c8db8a6d
Boris <bschapira@clever-age.com>
2014-06-09 14:14:47 +0200

    Bascule sur JoliTypo

--- typofr.php
+++ typofr.php
@@ -6,64 +6,35 @@ Description: a plugin for french typography management, inspired by SPIP
 Version: 0.1
 Author: Boris Schapira
 Author URI: http://borisschapira.com
-License: GPL2
+License: MIT
 */
 
-// Correction typographique francaise
-// Function fortement inspirée du travail de Arnaud Martin, Antoine Pitrou,
-// Philippe Riviere, Emmanuel Saint-James sur SPIP
+include 'vendor/autoload.php';
+
+// Correction typographique francaise par JoliTypo
+
+use JoliTypo\Fixer;
+
 function typofr($text)
 {
-    /** @var array $pairs Pairs of strings to replace * */
-    static $pairs;
-
-    /** @var string $joker A string that should not exist */
-    $joker = "-\x2-";
-
-    if (!isset($pairs)) {
-        $pairs = array(
-            "&nbsp;" => '~', // on utilise le ~ comme joker d'espace insécable
-            "&raquo;" => '&#187;', // »
-            "&laquo;" => '&#171;', // «
-            "&rdquo;" => '&#8221;', // ”
-            "&ldquo;" => '&#8220;', // “
-            "&deg;" => '&#176;', // degree
-            "'" => '&#8217;' // apostrophe
-        );
+    static $fixer;
+    if (!isset($fixer)) {
+        $fixer = new Fixer(array(
+//            'Ellipsis',
+//            'Dimension',
+//            'Dash',
+//            'FrenchQuotes',
+//            'FrenchNoBreakSpace',
+//            'CurlyQuote',
+//            'Hyphen',
+            'Trademark'));
+        $fixer->setLocale('fr_FR'); // Needed by the Hyphen Fixer
     }
 
-    $text = str_replace(array_keys($pairs), array_values($pairs), $text);
-
-    // S'il y a un ; dans le texte, on doit normalement le traiter,
-    // mais on risque de toucher les entitées &xxx; donc on réalise
-    // d'abord ce remplacement
-    if (strpos($text, ';') !== false) {
-        $text = str_replace(';', '~;', $text); // tout ; doit être précédé d'un espace insecable
-        $text = preg_replace(',(&#?[0-9a-z]+)~;,iS', '$1;', $text);; // sauf s'il fait partie d'une entité &xxx;
-    }
-
-    $text = preg_replace('/[!?][!?\.]*/S', "$joker~$0", $text, -1, $c);
-    if ($c) {
-        $text = preg_replace("/([\[<\(!\?\.])$joker~/S", '$1', $text);
-        $text = str_replace("$joker", '', $text);
-    }
-
-    $text = preg_replace('/&#171;|M(?:M?\.|mes?|r\.?|&#176;) |[nN]&#176; /S', '$0~', $text);
-
-    if (strpos($text, '~') !== false)
-        $text = preg_replace("/ *~+ */S", "~", $text);
-
-    $text = preg_replace("/--([^-]|$)/S", "$joker&mdash;$1", $text, -1, $c);
-    if ($c) {
-        $text = preg_replace("/([-\n])$joker&mdash;/S", "$1--", $text);
-        $text = str_replace($joker, '', $text);
-    }
-
-    $text = preg_replace(',(' ._PROTOCOLES_STD . ')~((://[^"\'\s\[\]\}\)<>]+)~([?]))?,S', '$1$3$4', $text);
-    $text = str_replace('~', '&nbsp;', $text);
-
-    return $text;
+    $fixed = $fixer->fix($text);
+    return $text."<script>console && console.log('-------');console && console.log('".$text." (".mb_detect_encoding($text).")'); console && console.log('".$fixed." (".mb_detect_encoding($fixed).")')</script>";
 }
 
-add_filter('the_content', 'typofr');
+//add_filter('the_content', 'typofr');
 add_filter('the_title', 'typofr');
+
