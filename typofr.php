a5c2cd73cfa0c93d32c055bced07a3c463d9c4f4
Boris <bschapira@clever-age.com>
2014-06-09 15:45:28 +0200

    Works + logs

--- typofr.php
+++ typofr.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: TypoFR
 Plugin URI: https://github.com/borisschapira/typofr
-Description: a plugin for french typography management, inspired by SPIP
+Description: a plugin for french typography management, powered by JoliTypo
 Version: 0.1
 Author: Boris Schapira
 Author URI: http://borisschapira.com
@@ -20,19 +20,26 @@ function typofr($text)
     static $fixer;
     if (!isset($fixer)) {
         $fixer = new Fixer(array(
-//            'Ellipsis',
-//            'Dimension',
-//            'Dash',
-//            'FrenchQuotes',
-//            'FrenchNoBreakSpace',
-//            'CurlyQuote',
-//            'Hyphen',
+            'Ellipsis',
+            'Dimension',
+            'Dash',
+            'FrenchQuotes',
+            'FrenchNoBreakSpace',
+            'CurlyQuote',
+            'Hyphen',
             'Trademark'));
         $fixer->setLocale('fr_FR'); // Needed by the Hyphen Fixer
     }
 
-    $fixed = $fixer->fix($text);
-    return $text."<script>console && console.log('-------');console && console.log('".$text." (".mb_detect_encoding($text).")'); console && console.log('".$fixed." (".mb_detect_encoding($fixed).")')</script>";
+    $decoded = utf8_decode($text);
+    $reEncoded = utf8_encode($decoded);
+
+    $fixed = $fixer->fix($decoded);
+    $fixedWrong = $fixer->fix($reEncoded);
+
+    $logs = "<script> console && console.log('-------\\nOriginal : ".$text." (".mb_detect_encoding($text).")\\nDecoded : ".$decoded." (".mb_detect_encoding($decoded).")\\nRe-encoded : ".$reEncoded." (".mb_detect_encoding($reincoded).")\\nFix(Decoded) : ".$fixed." (".mb_detect_encoding($fixed).")\\nFix(Re-encoded) : ".$fixedWrong." (".mb_detect_encoding($fixedWrong).")')</script>";
+
+    return $fixed.$logs;
 }
 
 //add_filter('the_content', 'typofr');
