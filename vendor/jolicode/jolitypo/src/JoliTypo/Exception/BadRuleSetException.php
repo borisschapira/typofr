82c1dbb39894695f675a03f53ebc47d5b4977915
Boris <bschapira@clever-age.com>
2014-06-11 08:37:25 +0200

    No need to store vendors in GIT (only needed in SVN Wordpress Plugins repo)

--- vendor/jolicode/jolitypo/src/JoliTypo/Exception/BadRuleSetException.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace JoliTypo\Exception;
-
-class BadRuleSetException extends \Exception
-{
-    protected $message = "RuleSet must be an array of Fixer names or instances.";
-}
--- vendor/jolicode/jolitypo/src/JoliTypo/Exception/InvalidMarkupException.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace JoliTypo\Exception;
-
-class InvalidMarkupException extends \DOMException
-{
-    protected $message = "An error happened when trying to read your HTML with \\DOMDocument.";
-}
