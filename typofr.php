3a2113676103230194c287c90c89f9498f8b3adc
Boris <bschapira@clever-age.com>
2014-08-13 16:46:05 +0200

    Wordpress plugin version increment

--- typofr.php
+++ typofr.php
@@ -5,7 +5,7 @@
  * Description: a plugin for french typography management, powered by JoliTypo
  *
  * Plugin URI: https://github.com/borisschapira/typofr
- * Version: 0.5
+ * Version: 0.6
  *
  * Author: Boris Schapira
  * Author URI: http://borisschapira.com
